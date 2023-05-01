203cbda216ef397fde8bbff30f88ce76afc62be9
NISHIGUCHI.K <112517270+nishguchi@users.noreply.github.com>
2023-05-01 12:08:26 +0900

    Structure
    構造体

---
+++ C20.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct
+{
+	char name[20];
+	int age;
+	char kigou;
+} person_t;
+
+
+int main(int argc, char *argv[])
+{
+	
+person_t person1 = {"太郎", 10, 'A'};
+person_t person2 = {"次郎", 11, 'B'};
+person_t person3 = {"三郎", 12, 'C'};
+person_t person4 = {"四郎", 13, 'D'};
+	
+
+
+printf("名前は%s、年齢は%d、型は%c\n", person1.name, person1.age, person1.kigou);
+printf("名前は%s、年齢は%d、型は%c\n", person2.name, person2.age, person2.kigou);
+printf("名前は%s、年齢は%d、型は%c\n", person3.name, person3.age, person3.kigou);
+printf("名前は%s、年齢は%d、型は%c\n", person4.name, person4.age, person4.kigou);
+
+}
+
